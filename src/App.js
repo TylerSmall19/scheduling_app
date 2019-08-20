4e239cd97bbcf1f57baa4a0a5bd2efccdf41f26a
Tyler Small <tylersmall@Tylers-MacBook-Pro.local>
2019-08-20 15:07:03 -0500

    Edit the app.js to trigger a deploy

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          Schedule a pool match!
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
