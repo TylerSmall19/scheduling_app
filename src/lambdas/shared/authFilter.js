f837c48cff9a277f213cb994d7528a6ea17900a6
Tyler Small <tylersmall@Tylers-MacBook-Pro.local>
2019-08-22 17:17:42 -0500

    Add the auth filter to the shared directory in lambdas

---
+++ src/lambdas/shared/authFilter.js
@@ -0,0 +1,16 @@
+export const isLoggedIn = (event, context, callback) => {
+  console.log("protected function!");
+  // Reading the context.clientContext will give us the current user
+  const claims = context.clientContext && context.clientContext.user;
+  console.log("user claims", claims);
+
+  if (!claims) {
+    console.log("User not logged in, or empty claims: ", claims);
+    callback(Error('User is not logged in'));
+    return;
+  }
+
+  callback(null, {statusCode: 401, body: 'Message'});
+
+  return true;
+}
