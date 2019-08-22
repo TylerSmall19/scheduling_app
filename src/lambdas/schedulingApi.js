e87ea4fc5b054011abf195eb9d48339e493124f1
Tyler Small <tylersmall@Tylers-MacBook-Pro.local>
2019-08-22 17:18:27 -0500

    Add logging and a call to the auth for testing

--- src/lambdas/schedulingApi.js
+++ src/lambdas/schedulingApi.js
@@ -1,3 +1,12 @@
-exports.handler = function () {
-  console.log('Help! I\'ve been involked!')
+import { isLoggedIn } from './shared/authFilter';
+
+exports.handler = function (event, context, callback) {
+  const loggedIn = isLoggedIn(event, context, callback);
+
+  if (loggedIn) {
+    console.log('Legally invoked!', JSON.stringify(context));
+    return JSON.stringify({ statusCode: 200, body: 'Welcome!' });
+  }
+
+  throw new Error();
 }
