21740fa0d40899cd93885dc690cfae81cb218abe
Tyler Small <tylersmall.user@gmail.com>
2019-09-09 14:59:05 -0500

    Update the scheduling API function

--- src/lambdas/schedulingApi.js
+++ src/lambdas/schedulingApi.js
@@ -4,8 +4,7 @@ exports.handler = async function (event, context, callback) {
   const loggedIn = isLoggedIn(event, context, callback);
 
   if (loggedIn) {
-    console.log('Logged in user quering scheduling API: ')
-    return { statusCode: 200, body: JSON.stringify({ msg: 'Welcome!' }) };
+    return { statusCode: 200, body: JSON.stringify({ msg: 'Welcome!', key: process.env.API_LOOKUP_KEY }) };
   }
 
   console.log('This was invoked illegally');
