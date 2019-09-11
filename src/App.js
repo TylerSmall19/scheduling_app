13e6eac762e56f1297e0191812b0ab00eaa82b64
Tyler Small <tylersmall.user@gmail.com>
2019-09-11 14:25:40 -0500

    Begin to lay groundwork for routing and content on the home page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './styles/App.css';
 import { IdentityContextProvider, useIdentityContext } from 'react-netlify-identity';
 import 'react-netlify-identity-widget/styles.css';
 import { schedulingAPI } from './services/schedulingAPIService';
+import { Router, Link } from "@reach/router"
+
 // Lazy load the module when login is attempted
 const IdentityModal = React.lazy(() => import('react-netlify-identity-widget'));
 
-function Login() {
+function Header() {
   const identity = useIdentityContext();
   const [dialog, setDialog] = React.useState(false);
   const isLoggedIn = identity && identity.isLoggedIn;
 
   return (
-    <div className='login-container col-2 col-xs-12 mx-auto float-left'>
-      <span className="login-btn" onClick={() => setDialog(true)}>
-        {isLoggedIn ? "LOG OUT" : "LOGIN"}
-      </span>
+    <div className='col-12'>
+      <div className='login-container col-3 col-xs-12'>
+        <span>
+          <Link to='teams/new' className=''>Create A Team</Link>
+        </span>
+      </div>
 
-      <React.Suspense fallback="loading...">
+      <div className='login-container col-2 col-xs-12'>
+        <span className="login-btn" onClick={() => setDialog(true)}>
+          {isLoggedIn ? "LOG OUT" : "LOGIN"}
+        </span>
+      </div>
+
+      <div>
+        
+      </div>
+
+      <React.Suspense fallback="">
         <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
       </React.Suspense>
     </div>
@@ -37,37 +51,60 @@ const HitAPI = () => {
   );
 }
 
+const NotFound = () => (
+  <span className='col-2 mx-auto'>Not Found</span>
+)
+
+const TeamsIndex = () => {
+  return (
+    <h1>Create a New Team</h1>
+  )
+}
+
+const AppIndex = () => {
+  return(
+    <div className="App">
+      <div className='container'>
+        <div className='row'>
+          <Header />
+          
+          <div className='col-12'>
+
+            <Router>
+              <TeamsIndex path='teams/new' />
+              <NotFound default />
+            </Router>
+
+            {/* <div className='mt-3'>
+              <h1>
+                Scheduling App
+              </h1>
+              <br />
+              <HitAPI />
+              <br /> <br />
+              <h2>
+                <Link to='home'>
+                  Schedule a pool match!
+                </Link>
+              </h2>
+            </div> */}
+          </div>
+        </div>
+      </div>
+      <span>
+        {window.location.hostname}
+      </span>
+    </div>
+  );
+}
+
 function App() {
   const url = 'https://schedule.tylerjsmall.com/';
   return (
     <IdentityContextProvider url={url}>
-      <div className="App">
-        <div className='container'>
-          <div className='row background'>
-            <div className='col-12'>
-              <Login />
-
-              <br />
-
-              <h1>Schedule App</h1>
-
-              <br />
-
-              <HitAPI />
-
-              <br />
-              <br />
-
-              <h2>
-                Schedule a pool match!
-              </h2>
-            </div>
-          </div>
-        </div>
-        <span>
-          {window.location.hostname}
-        </span>
-      </div>
+      <Router>
+        <AppIndex path='/*' />
+      </Router>
     </IdentityContextProvider>
   );
 }
