680a6a48b5fb2922f71eb4a8c3a49d8e2fe47aea
Tyler Small <tylersmall.user@gmail.com>
2019-09-12 15:49:47 -0500

    Pull the header out from app.js to cleanup; Update the collapse logic for most of the links in the header

--- src/App.js
+++ src/App.js
@@ -1,72 +1,12 @@
 import React from 'react';
 import 'react-netlify-identity-widget/styles.css';
 import './styles/App.css';
-import { IdentityContextProvider, useIdentityContext } from 'react-netlify-identity';
-import { schedulingAPI } from './services/schedulingAPIService';
-import { Router, Link } from '@reach/router'
-import {Container, Col, Navbar, Nav } from 'react-bootstrap';
+import { IdentityContextProvider } from 'react-netlify-identity';
+import { Router } from '@reach/router'
+import { Container, Col } from 'react-bootstrap';
 import { NewTeamPage } from './components/pages/NewTeamPage';
 import { HomePage } from './components/pages/HomePage';
-
-// Lazy load the module when login is attempted
-const IdentityModal = React.lazy(() => import('react-netlify-identity-widget'));
-
-const AuthBtn = (props) => {
-  const identity = useIdentityContext();
-  const [dialog, setDialog] = React.useState(false);
-  const isLoggedIn = identity && identity.isLoggedIn;
-
-  return (
-    <React.Fragment>
-      <span {...props} onClick={() => setDialog(true)}>
-        {isLoggedIn ? 'LOG OUT' : 'LOGIN'}
-      </span>
-
-      <React.Suspense fallback=''>
-        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
-      </React.Suspense>
-    </React.Fragment>
-  );
-}
-
-function Header() {
-  return (
-    <React.Fragment>
-      <Navbar 
-        bg='dark' 
-        expand='md' 
-        variant='dark' 
-        fixed='top'
-        collapseOnSelect={true}
-      >
-        <Navbar.Brand as={(props) => <Link {...props} to='/'>Team Scheduling</Link>} />
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='mr-auto'>
-            <Nav.Link as={(props) => <Link {...props} to='/'>Home</Link>} />
-            <Nav.Link as={(props) => <Link {...props} to='teams/new'>Create a Team</Link>} />
-          </Nav>
-          <Nav>
-            <Nav.Link as={AuthBtn} />
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      </React.Fragment>
-  )
-}
-
-const HitAPI = () => {
-  const identity = useIdentityContext();
-
-  const onClickHandler = async () => {
-    const res = await new schedulingAPI(identity).getTeamSchedule('1');
-    console.log(res)
-  }
-
-  return (
-    <button onClick={onClickHandler} className='btn-secondary'>Test</button>
-  );
-}
+import { Header } from './components/navigation/header';
 
 const NotFound = () => (
   <span className='col-2 mx-auto'>Not Found</span>
---
+++ src/components/navigation/header.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { useIdentityContext } from 'react-netlify-identity';
+import { schedulingAPI } from '../../services/schedulingAPIService';
+import { Link } from '@reach/router'
+import {Navbar, Nav } from 'react-bootstrap';
+
+// Lazy load the module when login is attempted
+const IdentityModal = React.lazy(() => import('react-netlify-identity-widget'));
+
+const HitAPI = () => {
+  const identity = useIdentityContext();
+
+  const onClickHandler = async () => {
+    const res = await new schedulingAPI(identity).getTeamSchedule('1');
+    console.log(res)
+  }
+
+  return (
+    <button onClick={onClickHandler} className='btn-secondary'>Test</button>
+  );
+}
+
+const AuthBtn = (props) => {
+  const identity = useIdentityContext();
+  const [dialog, setDialog] = React.useState(false);
+  const isLoggedIn = identity && identity.isLoggedIn;
+
+  return (
+    <React.Fragment>
+      <Link {...props} to={window.location.pathname} onClick={() => setDialog(true)}>
+        {isLoggedIn ? 'LOG OUT' : 'LOGIN'}
+      </Link>
+
+      <React.Suspense fallback=''>
+        <IdentityModal showDialog={dialog} onCloseDialog={() => setDialog(false)} />
+      </React.Suspense>
+    </React.Fragment>
+  );
+}
+
+export const Header = () => {
+  return (
+    <React.Fragment>
+      <Navbar 
+        bg='dark' 
+        expand='md' 
+        variant='dark' 
+        fixed='top'
+        collapseOnSelect={true}
+      >
+        <Navbar.Brand as={(props) => <Link {...props} to='/'>Team Scheduling</Link>} />
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/' as={(props) => <Link {...props} to='/'>Home</Link>} />
+            <Nav.Link href='#' as={(props) => <Link {...props} to='teams/new'>Create a Team</Link>} />
+          </Nav>
+          <Nav>
+            <Nav.Link href={window.location.pathname} as={AuthBtn} />
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      </React.Fragment>
+  )
+}
+
