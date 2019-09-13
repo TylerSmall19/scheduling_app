ec16e5526787550b90a06c5d97acd538ce760136
Tyler Small <tylersmall.user@gmail.com>
2019-09-12 20:20:54 -0500

    Create a global routes file

---
+++ src/constants/appRoutes.js
@@ -0,0 +1,12 @@
+export const appRoutes = {
+  home: () => '/',
+  newTeam: () => '/teams/new',
+  teamPage: (id) => `/teams/${id}`
+}
+
+const schedApiBase = '/.netlify/functions';
+export const schedulingAPIRoutes = {
+  root: () => schedApiBase,
+  teamSchedule: (teamID) => schedApiBase + '/schedulingApi?teamID=' + teamID,
+  createNewTeam: () => schedApiBase + '/teams'
+}
