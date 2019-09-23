540f3168db1d2c13013f780fa4579e74cf409b06
Tyler Small <tylersmall.user@gmail.com>
2019-09-23 17:15:46 -0500

    Add the protocol to the default root URL

--- src/lambdas/shared/lambdaConfigManager.js
+++ src/lambdas/shared/lambdaConfigManager.js
@@ -1,5 +1,5 @@
 // This is set in the netlify console in a similar way to the API Keys
-const root = process.env.SCHEDULING_API_ROOT || 'localhost:3001';
+const root = process.env.SCHEDULING_API_ROOT || 'http://localhost:3001';
 
 export default {
   schedulingApiKey: process.env[process.env.API_LOOKUP_KEY] || 'local',
