e0111de59b335c4058a4d178d02f09675720d38c
Tyler Small <tylersmall.user@gmail.com>
2019-09-23 17:16:19 -0500

    Add in some error handling and error masking for the front end coming from the API

--- src/lambdas/createNewTeam.js
+++ src/lambdas/createNewTeam.js
@@ -1,10 +1,24 @@
 import { isLoggedIn } from './shared/authFilter';
+import { OutgoingAPIClient } from './shared/lambdaOutgoingAPIClient';
+import { internalError, successResponse } from './shared/responses';
 
 exports.handler = async function (event, context, callback) {
   const loggedIn = isLoggedIn(event, context, callback);
 
   if (loggedIn) {
-    return { statusCode: 200, body: JSON.stringify({ event, context, callback }) };
+    console.log(context.clientContext);
+
+    return OutgoingAPIClient.createNewTeam(event.body, context.clientContext.sub)
+      .then(res => {
+        if (res.status >= 200 && res.status < 300)
+          return successResponse(res);
+        else
+          throw new Error(JSON.stringify(res.exception));
+      })
+      .catch(ex => {
+        console.error(ex);
+        return internalError();
+      });
   }
 
   console.log('This was invoked illegally');
