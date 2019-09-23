0e0d1683b8831f3b11d6aaf845e49edab2927ee2
Tyler Small <tylersmall.user@gmail.com>
2019-09-23 16:03:39 -0500

    Add an outgoing client for external communication (basically a fetch wrapper for v1)

---
+++ src/lambdas/shared/lambdaOutgoingAPIClient.js
@@ -0,0 +1,27 @@
+import ConfigManager from './lambdaConfigManager';
+
+// const postData('http://example.com/answer', {answer: 42})
+// .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
+// .catch(error => console.error(error));
+
+export const communicationClient = (url = '', method = 'GET', bodyData = null) => {
+  let options = {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      'api_key': ConfigManager.schedulingApiKey
+    }
+  };
+
+  if (bodyData)
+    options.body = JSON.stringify(bodyData)
+
+  return fetch(url, options)
+    .then(response => response.json());
+}
+
+export const OutgoingAPIClient = {
+  async createNewTeam(values) {
+    return await communicationClient(ConfigManager.externalAPIRoutes.createTeam, 'POST', values);
+  }
+}
