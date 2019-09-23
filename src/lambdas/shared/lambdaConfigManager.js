730a5c36a283fc6fa43e5b2609dc89ed91c23a22
Tyler Small <tylersmall.user@gmail.com>
2019-09-23 16:05:08 -0500

    Add more configuration to the manager. This now includes routes for the API

--- src/lambdas/shared/lambdaConfigManager.js
+++ src/lambdas/shared/lambdaConfigManager.js
@@ -1,3 +1,10 @@
+// This is set in the netlify console in a similar way to the API Keys
+const root = process.env.SCHEDULING_API_ROOT || 'localhost:3001';
+
 export default {
-  schedulingApiKey: process.env[process.env.API_LOOKUP_KEY] || 'local'
+  schedulingApiKey: process.env[process.env.API_LOOKUP_KEY] || 'local',
+  externalAPIRoutes: {
+    root,
+    createTeam: () => root + '/teams'
+  }
 }
